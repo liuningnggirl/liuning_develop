894c2d06ca4b518ecd203786c4e576441642a3ab
ningliu <3221224168@qq.com>
2019-09-03 15:31:09 +0800

    del master内容

--- nggirl/nggirlh5/h5/mobile/js/focuscontent.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-	$('.telement').live('touchstart',function(){
-		if($(this).attr('superlink') != null && $(this).attr('superlink') != ''){
-			window.location.href = $(this).attr('superlink');
-		}
-	});
-});
